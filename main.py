1295467b48cbd5d3bee2c737177a774ac724cb0a
Daniel Alegria Sallo <ccusdanielalegriasallo@gmail.com>
2023-01-28 09:15:36 -0500

    added flask code in main.py and added templates/

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+from flask import Flask, render_template
 
-import organizador
+app = Flask(__name__)
+
+## create a route decorator
+@app.route ( '/' )
+def index():
+    return render_template("index.html")
+
+
+@app.route ( '/flight_board/' )
+def flight_board():
+    return render_template("flight_board.html")
+
+
+@app.route ( '/users/' )
+def users():
+    return render_template("users.html")
